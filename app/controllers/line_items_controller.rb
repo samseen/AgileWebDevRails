389af3346c087c4d7dde970af662e49e4c6d3b41
Samson Akanbi <samson.akanbi@outlook.com>
2016-10-13 10:00:56 +0100

    In this chapter:10, I added a column to an existing table with a default value, Migrated existing data into the new table format, Provided a flash notice of an error that was detected, Used the logger to log events, deleted a record and Adjusted the way a table is rendered using CSS

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,10 +30,10 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.json { render json: @line_item }
+        format.html { redirect_to @line_item.cart }
+        format.json { render json: @line_item, status: :created, location: @line_item }
       else
-        format.html { render :new }
+        format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
