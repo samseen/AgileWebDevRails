17da524baaecec71f45a5e12e6e4f6f28c4bc5ae
Samson Akanbi <samson.akanbi@outlook.com>
2016-10-21 10:19:49 +0100

    We configured our development, test, and production environments for our Rails application to enable the sending of outbound emails. We created and tailored a mailer that will send confirmation emails in both plain-text and HTML formats to people who order our products. We created both a functional test for the emails produced, as well as an integration test that covers the entire order scenario.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -47,13 +47,16 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render json: @order, status: :created, location: @order }
+        OrderNotifier.received(@order).deliver
+        format.html { redirect_to store_url, notice:
+         'Thank you for your order.' }
+        format.json { render json: @order, status: :created,
+         location: @order }
       else
         @cart = current_cart
         format.html { render action: "new" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @order.errors, 
+          status: :unprocessable_entity }
       end
     end
   end
